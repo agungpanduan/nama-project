467e70f53d0e7afa0ee9b727710e8a80501623ff
Agung <Source>
2020-06-11 18:03:23 +0700

    Test Commit and Pull to GIthub by AgungPanduan.com

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logika_Basic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] array1 = { 0, 1, 2, 3, 4, 5 };
+            
+            for (int i = 1; i < 6; i++)
+            {
+                System.Console.WriteLine(array1[i].ToString());
+                array1.a
+            }
+            
+        }
+    }
+}
